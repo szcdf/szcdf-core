3b0c9fcaca952d716aa20f90bfc29b50905d7815
Stephen Zhao <mail@zhaostephen.com>
2025-11-28 01:27:07 -0500

    Add Sudlander profile

---
+++ src/core-presets/set-shell-prompt-powerline-red.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+###############################################################################
+#
+# Package: szcdf-core
+# Author: Stephen Zhao (mail@zhaostephen.com)
+# Type: Preset
+# Preset: set-shell-prompt-powerline-red
+# Purpose: Sets up the powerline prompt in red.
+
+szcdf_logging__begin_context 'core-presets/set-shell-prompt-powerline-red'
+
+szcdf_module_manager load colors
+szcdf_module_manager load powerline
+
+szcdf_logging__debug "Setting up PS1..."
+
+if ! szcdf_shinter return_is_interactive; then
+
+  szcdf_logging__info "Shell is not interactive. The set-shell-prompt-powerline-red only applies when the shell is interative. Skipping..."
+
+  szcdf_logging__end_context
+
+  return 0
+
+fi
+
+szcdf_powerline set_ps1_by_color_scheme RED
+
+szcdf_logging__debug "Finished setting up PS1."
+
+szcdf_logging__end_context
---
+++ src/core-profiles/Sudlander__WSL/on_load.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+###############################################################################
+#
+# Package: szcdf-core
+# Profile: Sudlander__WSL
+# Author: Stephen Zhao (mail@zhaostephen.com)
+# Type: Profile
+# Purpose: The Sudlander__WSL profile for szcdf-core.
+
+szcdf_logging__begin_context 'core-profiles/Sudlander__WSL'
+
+szcdf_logging__debug "Registering presets for profile 'Sudlander__WSL'..."
+
+szcdf_preset register add-bash-recommended
+szcdf_preset register add-wsl-recommended
+szcdf_preset register add-zhaonetwork1-accessor
+szcdf_preset register add-vim-managed
+szcdf_preset register add-conda-recommended
+szcdf_preset register add-nodejs-dev
+szcdf_preset register add-ssh-ids-to-keychain
+szcdf_preset register use-nord-theme
+szcdf_preset register set-shell-prompt-powerline-red
+
+szcdf_logging__debug "Finished registering presets for profile 'Sudlander__WSL'."
+
+szcdf_logging__debug "Adding other env variables..."
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+szcdf_logging__debug "Finished adding other env variables."
+
+szcdf_logging__debug "Adding some common aliases..."
+
+alias cdprl='cd /home/stephen/projects'
+alias cdprw='cd /mnt/c/Projects'
+
+szcdf_module_manager load link_syncer
+szcdf_link_syncer ensure_link \
+  "$SZCDF_G__ROOT_DIR/data/mail_zhaostephen_com.gitconfig" \
+  "$HOME/.gitconfig"
+
+szcdf_module_manager load text_section_syncer
+szcdf_text_section_syncer ensure_text_section_or_prepend \
+  "$HOME/.ssh/config" \
+  "$SZCDF_G__ROOT_DIR/data/mail_zhaostephen_com.github.sshconfig" \
+  "SSHCONFIG_GITHUB_MAIL_ZHAOSTEPEN_COM"
+
+szcdf_logging__debug "Finished adding some common aliases."
+
+szcdf_logging__end_context

ce644f8619ac4c3c41d040d025d7ec8f1fd8b931
Stephen Zhao <mail@zhaostephen.com>
2026-06-17 01:02:28 -0400

    Add some aliases for Nordlander

--- src/core-profiles/Nordlander__WSL/on_load.sh
+++ src/core-profiles/Nordlander__WSL/on_load.sh
@@ -35,10 +35,21 @@ szcdf_logging__debug "Adding some common aliases..."
 alias cdprl='cd /home/stephen/projects'
 alias cdprw='cd /mnt/e/Projects/Dev'
 
+alias firefox='powershell.exe -Command "& \"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Firefox.lnk\""'
+alias discord='powershell.exe -Command "& \"C:\Users\Stephen Zhao\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord.lnk\""'
+
+szcdf_logging__debug "Finished adding some common aliases."
+
+szcdf_logging__debug "Adding symlinks..."
+
 szcdf_module_manager load link_syncer
 szcdf_link_syncer ensure_link \
   "$SZCDF_G__ROOT_DIR/data/mail_zhaostephen_com.gitconfig" \
   "$HOME/.gitconfig"
+
+szcdf_logging__debug "Finished adding symlinks."
+
+szcdf_logging__debug "Adding text sections..."
 
 szcdf_module_manager load text_section_syncer
 szcdf_text_section_syncer ensure_text_section_or_prepend \
@@ -46,6 +57,6 @@ szcdf_text_section_syncer ensure_text_section_or_prepend \
   "$SZCDF_G__ROOT_DIR/data/mail_zhaostephen_com.github.sshconfig" \
   "SSHCONFIG_GITHUB_MAIL_ZHAOSTEPEN_COM"
 
-szcdf_logging__debug "Finished adding some common aliases."
+szcdf_logging__debug "Finished adding text sections."
 
 szcdf_logging__end_context
